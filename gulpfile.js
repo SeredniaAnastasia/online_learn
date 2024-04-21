1928184bb3c5918a3e8229539bf0eccd172f2e51
SeredniaAnastasia <113719857+SeredniaAnastasia@users.noreply.github.com>
2024-04-21 19:41:10 +0300

    finish commit

--- gulpfile.js
+++ gulpfile.js
@@ -42,10 +42,13 @@ function scripts(){
        'node_modules/rateyo/src/jquery.rateyo.js', //рейтингами звезд
       // 'node_modules/ion-rangeslider/js/ion.rangeSlider.js', //Плагин ion.rangeSlider  динамически менять шаг ползунка?
       // 'node_modules/jquery-form-styler/dist/jquery.formstyler.js', //плагин для стилизации селектов, чекбоксов, радиокнопок, файловых и числовых полей
+      'app/js/send-mail.js',
+      'app/js/validate.js',
+      'app/js/send-newsletter.js',
       'app/js/main.js'
     ])  
      .pipe(concat('main.min.js'))
-    .pipe(uglify()) // мініиізує файл
+     .pipe(uglify()) // мініиізує файл
      .pipe(dest('app/js'))
      .pipe(sync.stream())
 }
